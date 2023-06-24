796281b9630b71546bb9eaffd9f350af5b870fc7
azadei <135730696+azadei@users.noreply.github.com>
2023-06-24 13:46:31 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    public class person
+    {
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string age { get; set; }
+    }
+
+    public class teacher : person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("Explaining...");
+        }
+    }
+
+    public class student : person
+
+    {
+        public void Learn()
+        {
+            Console.WriteLine("Learning...");
+
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            teacher teacher = new teacher();
+            student student = new student();
+
+            teacher.name = "name";
+            teacher.surname = "surname";
+            teacher.age = "21";
+
+            Console.WriteLine(teacher.name + "  " + teacher.surname + "  " + teacher.age);
+            teacher.Explain();
+
+            student.name = "name";
+            student.surname = "surname";
+            student.age = "31";
+
+            Console.WriteLine(student.name + "  " + student.surname + "  " + student.age);
+            student.Learn();
+
+
+        }
+
+    }
+}
